e9c938d05399ba29bb81ac5c270f7748b1865257
Adi Mahluf <68636014+AdiM10root@users.noreply.github.com>
2024-02-29 13:06:38 +0200

    Additional files for creating OUs and Users

---
+++ 2_CreateOUs.ps1
@@ -0,0 +1,15 @@
+﻿#Create OUs
+Set-Location c:\vagrant
+$users = Import-Csv -Path .\3000users.csv
+
+foreach ($user in $users) {
+    # Check if the OU exists
+    $ou = $user.Department
+    if ($ou -like $null) {
+    return}
+    $ouExists = Get-ADOrganizationalUnit -Filter "DistinguishedName -eq '$ou'" -ErrorAction SilentlyContinue
+    if (-not $ouExists) {
+        # Create the OU if it does not exist
+        New-ADOrganizationalUnit -Name $ou -ProtectedFromAccidentalDeletion $false -ErrorAction SilentlyContinue
+    }
+}
---
+++ 3_CreateUsers.ps1
@@ -0,0 +1,2 @@
+﻿#Create Users
+Import-csv .\3000users.csv | New-ADUser -AccountPassword (ConvertTo-SecureString '1234qwer!@#$' -AsPlainText -Force) -Enabled $true
