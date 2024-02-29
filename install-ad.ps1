dc7d1db68022ad41ea09aaa4dff97942f685ae91
Adi Mahluf <68636014+AdiM10root@users.noreply.github.com>
2024-02-29 11:11:23 +0200

    Create install-ad.ps1

---
+++ install-ad.ps1
@@ -0,0 +1,5 @@
+﻿Set-ExecutionPolicy Bypass -Scope Process -Force
+Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
+Import-Module ADDSDeployment
+$securePassword = ConvertTo-SecureString '1234qwer!@#$' -AsPlainText -Force
+Install-ADDSForest -CreateDnsDelegation:$false -DatabasePath 'C:\Windows\NTDS' -DomainMode 'WinThreshold' -DomainName 'contoso.local' -DomainNetbiosName 'contoso' -ForestMode 'WinThreshold' -InstallDns:$true -LogPath 'C:\Windows\NTDS' -NoRebootOnCompletion:$false -SysvolPath 'C:\Windows\SYSVOL' -Force:$true -safeModeAdministratorPassword $securePassword
