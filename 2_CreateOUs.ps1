ad98795052d8fec9b6d6fd1f198db2367fc282d4
Adi Mahluf <68636014+AdiM10root@users.noreply.github.com>
2024-03-26 18:04:55 +0300

    fixing errors
    not done yet with creatreusers

--- 2_CreateOUs.ps1
+++ 2_CreateOUs.ps1
@@ -1,21 +1,8 @@
-﻿$username = "contoso\vagrant"
+﻿$username = "vagrant@contoso.local"
 $password = "vagrant"
 $securePassword = ConvertTo-SecureString $password -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential ($username, $securePassword)
-Invoke-Command -Credential $credential -ScriptBlock {
-#Create OUs
-Set-Location c:\vagrant
-$users = Import-Csv -Path c:\vagrant\3000users.csv
 
-foreach ($user in $users) {
-    # Check if the OU exists
-    $ou = $user.Department
-    if ($ou -like $null) {
-    return}
-    $ouExists = Get-ADOrganizationalUnit -Filter "DistinguishedName -eq '$ou'" -ErrorAction SilentlyContinue
-    if (-not $ouExists) {
-        # Create the OU if it does not exist
-        New-ADOrganizationalUnit -Name $ou -ProtectedFromAccidentalDeletion $false -ErrorAction SilentlyContinue
-    }
-}
-}
+Start-Process powershell.exe -Credential $credential -ArgumentList "-Command", " `$users = Import-Csv -Path 'c:\vagrant\3000users.csv'; foreach (`$user in `$users) { `$ou = `$user.Department; if (`$ou -like `$null) { return }; `$ouExists = Get-ADOrganizationalUnit -Filter 'DistinguishedName -like `$ou' -ErrorAction SilentlyContinue; if (`$ouExists -eq `$null) { New-ADOrganizationalUnit -Name `$ou -ProtectedFromAccidentalDeletion `$false -ErrorAction SilentlyContinue } }`""
+
+
--- 3_CreateUsers.ps1
+++ 3_CreateUsers.ps1
@@ -1,8 +1,9 @@
-﻿$username = "contoso\vagrant"
+﻿$username = "vagrant@contoso.local"
 $password = "vagrant"
 $securePassword = ConvertTo-SecureString $password -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential ($username, $securePassword)
-Invoke-Command -Credential $credential -ScriptBlock {
-#Create Users
-Import-csv c:\vagrant\3000users.csv | New-ADUser -AccountPassword (ConvertTo-SecureString '1234qwer!@#$' -AsPlainText -Force) -Enabled $true
+
+Start-Process powershell.exe -Credential $credential -ArgumentList "-Command", " `Import-Csv c:\vagrant\3000users.csv | ForEach-Object { 
+    New-ADUser -Name $_.DisplayName -DisplayName $_.DisplayName -SamAccountName $_.SamAccountName -UserPrincipalName $_.UserPrincipalName -Department $_.Department -Path $_.Path -GivenName $_.GivenName -Surname $_.Surname -Title $_.Title -City $_.City -Company $_.Company -Country $_.Country -AccountPassword (ConvertTo-SecureString "1234qwer!@#$" -AsPlainText -Force) -Enabled $enabled 
 }
+"
