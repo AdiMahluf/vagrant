a5ee4cc93b3fb51b9382820096b2dc2274862db2
Adi Mahluf <68636014+AdiM10root@users.noreply.github.com>
2024-03-27 18:02:46 +0300

    fix create users script
    working manually

--- 3_CreateUsers.ps1
+++ 3_CreateUsers.ps1
@@ -3,7 +3,6 @@ $password = "vagrant"
 $securePassword = ConvertTo-SecureString $password -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential ($username, $securePassword)
 
-Start-Process powershell.exe -Credential $credential -ArgumentList "-Command", " `Import-Csv c:\vagrant\3000users.csv | ForEach-Object { 
-    New-ADUser -Name $_.DisplayName -DisplayName $_.DisplayName -SamAccountName $_.SamAccountName -UserPrincipalName $_.UserPrincipalName -Department $_.Department -Path $_.Path -GivenName $_.GivenName -Surname $_.Surname -Title $_.Title -City $_.City -Company $_.Company -Country $_.Country -AccountPassword (ConvertTo-SecureString "1234qwer!@#$" -AsPlainText -Force) -Enabled $enabled 
-}
-"
+Start-Process powershell.exe -Credential $credential -ArgumentList "-Command", "Import-Csv c:\vagrant\3000users.csv | ForEach-Object {
+    New-ADUser -Name `$_.DisplayName -DisplayName `$_.DisplayName -SamAccountName `$_.SamAccountName -UserPrincipalName `$_.UserPrincipalName -Department `$_.Department -Path `$_.Path -GivenName `$_.GivenName -Surname `$_.Surname -Title `$_.Title -City `$_.City -Company `$_.Company -Country `$_.Country -AccountPassword (ConvertTo-SecureString '1234qwer!@#$' -AsPlainText -Force) -Enabled `$true
+}"
